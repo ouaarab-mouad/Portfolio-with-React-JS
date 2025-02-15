1376585df0f339994c01d681728a8d3fbb62abe4
MOUAD AIT OUAARAB <152430253+ouaarab-mouad@users.noreply.github.com>
2025-02-15 21:04:23 +0100

    Add all file extensions here tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",  // Add all your file extensions here
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
